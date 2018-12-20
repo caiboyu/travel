a62a97cc2c97fa3adc56c356b65c0d5c69dd3d85
caiboyu <997248372@qq.com>
2018-12-20 17:02:45 +0800

    提交到详情页

--- src/pages/detail/components/banner.vue
+++ src/pages/detail/components/banner.vue
@@ -1,25 +1,74 @@
 <template>
-	<div class="banner">
-		<img class="banner-img" src="//img1.qunarzz.com/sight/p0/1704/c9/c936f3fccfc6d7eda3.img.jpg_600x330_1be7ea5e.jpg"/>
-		<div class="banner-info">
-			<div>上海杜莎夫人蜡像馆</div>
-			<div><span class="iconfont arrow-icon">&#xe61c;</span>28</div>
-			
+    <div>
+    	<div class="banner" @click="handleClickGallary">
+			<img class="banner-img" src="//img1.qunarzz.com/sight/p0/1704/c9/c936f3fccfc6d7eda3.img.jpg_600x330_1be7ea5e.jpg"/>
+			<div class="banner-info">
+				<div class="banner-title">上海杜莎夫人蜡像馆</div>
+				<div class="banner-number"><span class="iconfont banner-icon">&#xe791;</span>28</div>
+				
+			</div>
 		</div>
-	</div>
+		<common-gallary :imgs="imgs" v-show="showGallary" @close="handleGallaryClose"></common-gallary>
+    </div>
 </template>
 
 <script>
+import CommonGallary from "common/gallary/gallary"
 export default {
-	name: 'DetailBanner'
+	name: 'DetailBanner',
+	data () {
+		return {
+			showGallary: false,
+			imgs: [
+			    "http://img1.qunarzz.com/sight/p0/1704/66/668431aeff49b77ca3.img.jpg_r_800x800_25c929be.jpg",
+			    "http://img1.qunarzz.com/sight/p0/1703/22/2244eaab209bf750a3.img.jpg_r_800x800_d4d9e847.jpg",
+			    "http://img1.qunarzz.com/sight/p0/1704/bb/bb5bd93bb94c1d95a3.img.jpg_r_800x800_759a83aa.jpg" 
+			]
+		}
+	},
+	methods: {
+		handleClickGallary () {
+			this.showGallary = true
+		},
+		handleGallaryClose () {
+			this.showGallary = false
+		}
+	},
+	components: {
+		CommonGallary
+	}
 }
 </script>
 
 <style lang="stylus" scoped>
     .banner
+        position:relative
         overflow:hidden
         height:0
         padding-bottom:55%
         .banner-img
             width:100%
+        .banner-info
+            display:flex
+            position:absolute
+            left:0
+            right:0
+            bottom:0
+            line-height:.6rem
+            color:#fff
+            background-image:linear-gradient(top,rgba(0,0,0,0),rgba(0,0,0, 0.8))
+            .banner-title
+                flex:1
+                font-size:.32rem
+                padding:0 .2rem
+            .banner-number
+                padding: 0 .4rem
+                height:.32rem
+                line-height:.32rem
+                margin-top:.14rem
+                border-radius:.2rem
+                background:rgba(0, 0, 0, .8)
+                font-size:.24rem
+                .banner-icon
+                     font-size:.24rem
 </style>
---
+++ src/pages/detail/components/header.vue
@@ -0,0 +1,57 @@
+<template>
+   <div>
+	   <router-link tag="div" to="/" class="header-abs">
+	   	    <div class="iconfont header-abs-back">&#xe624;</div>
+	   </router-link>
+	   <div class="header-fixed">
+	   	    <div class="header">
+		        <router-link to="/">
+		        	<div class="iconfont header-back">&#xe624;</div>
+		        </router-link>
+		    </div>
+	   </div>
+	   
+   </div>
+</template>
+
+<script>
+export default {
+	name: "DetailHeader"
+}
+</script>
+
+<style lang="stylus" scoped>
+@import '~styles/varibles.styl'
+    .header-abs
+        position:absolute
+        top:.2rem
+        left:.2rem
+        width:.8rem
+        height:.8rem
+        line-height:.8rem
+        text-align:center
+        border-radius:.4rem
+        background:rgba(0, 0, 0, 0.8)
+        .header-abs-back
+            color:#fff
+            font-size:.4rem
+    .header-fixed
+        position:fixed
+        top:0
+        left:0
+        right:0
+        height:.86rem
+        line-height:.86rem
+        text-align:center
+        color:#fff
+        background:$bgColor
+        font-size:.32rem
+        .header-back
+            position:absolute
+            top:0
+            left:0
+            width:.64rem
+            text-align:center
+            font-size:.4rem
+            color:#fff
+</style>
